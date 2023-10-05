1a88cea65a7205f46107bc77146e95eda066c849
Anurag Patil <anuraggp2001@gmail.com>
2023-10-05 22:43:24 +0530

    added Timer() to find_primes_decorator.py

--- Assignment_day_11/find_primes_decorator.py
+++ Assignment_day_11/find_primes_decorator.py
@@ -1,11 +1,24 @@
 import time
+
+class Timer:
+    def __init__(self):
+        self.time_taken = None
+        self.start = None
+
+    def __enter__(self):
+        self.start = time.time()
+    
+    def __exit__(self, t, e, s):
+        end = time.time()
+        self.time_taken = end - self.start
+
 def performance_log(fn_target):
     def inner(*args, **kwargs):
         start_time = time.time()
         result = [x for x in fn_target(*args, **kwargs)]  
         end_time = time.time()
         time_taken = end_time-start_time
-        print(f'time taken: {time_taken}sec')
+        # print(f'time taken: {time_taken}sec')
         return result
     return inner
 
@@ -25,7 +38,13 @@ def find_primes(min, max=None):
         if is_prime(val):
             yield val
 
-x = find_primes(1000)
-print(x)
-print(len(x))
-            
+# x = find_primes(1000)
+# print(x)
+# print(len(x))
+
+t = Timer()
+with t:
+    p = find_primes(2, 20000)
+    print('total primes: ', len(p))
+print('total time taken', t.time_taken)
+
